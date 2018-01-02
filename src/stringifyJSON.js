3f1bffb385680c3e196ed4d2bacde6307c38a134
superhsu <31141265+superhsu@users.noreply.github.com>
2018-01-01 18:11:05 -0800

    practising psuedo code and adding comments to project

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,7 +3,10 @@
 
 // but you don't so you're going to write it from scratch:
 
-// inputs are anytype of data and the output would be a string version of the input. 
+/* inputs are anytype of data and the output would be a string version of the input. 
+   use if else statements to return string version of inputs that dont need loops 
+   use loop and recursion to iterate through object and array type inputs to return a string version*/ 
+
 
 var stringifyJSON = function(obj) {
   // your code goes here
