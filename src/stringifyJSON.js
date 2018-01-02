8654778ab2618ec3d2ecfbe1514654a7628f6d45
superhsu <31141265+superhsu@users.noreply.github.com>
2018-01-01 18:05:23 -0800

    first commit on stringifyJSON added comment and if else statements

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,13 @@
 
 // but you don't so you're going to write it from scratch:
 
+// inputs are anytype of data and the output would be a string version of the input. 
+
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'number') {
+  	return '' + obj; 
+  }
+
+  else if (typeof obj === '')
 };
